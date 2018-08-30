b8ea0b5b409b58f8a4dfd31b25b819a37d604b7c
keigawa <kei.ogawa@finc.com>
2018-08-30 16:01:05 +0900

    controller changed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,32 +2,31 @@ class QuestionsController < ApplicationController
   before_action :set_survey
 
   def new
-    @display_order=0
+    @display_order = 0
   end
 
   def create
-   @question=@survey.questions.create(question_params)
-   #p params[:type]
-   type=params[:type]
-    if type=="textbox"
-    redirect_to new_survey_question_textbox_path(@survey,@question)
-  elsif type=="textarea" 
-    redirect_to new_survey_question_textarea_path(@survey,@question)
-  elsif type=="checkbox"
-redirect_to new_survey_question_checkbox_path(@survey,@question)
-  elsif type=="radiobutton"
-redirect_to new_survey_question_radiobutton_path(@survey,@question)
+    @question = @survey.questions.create(question_params)
+    # p params[:type]
+    type = params[:type]
+    if type == 'textbox'
+      redirect_to new_survey_question_textbox_path(@survey, @question)
+    elsif type == 'textarea'
+      redirect_to new_survey_question_textarea_path(@survey, @question)
+    elsif type == 'checkbox'
+      redirect_to new_survey_question_checkbox_path(@survey, @question)
+    elsif type == 'radiobutton'
+      redirect_to new_survey_question_radiobutton_path(@survey, @question)
   end
   end
 
-private
-def set_survey
-  @survey=Survey.find(params[:survey_id])
+  private
+
+  def set_survey
+    @survey = Survey.find(params[:survey_id])
+  end
+
+  def question_params
+    params.permit(:number, :survey_id)
+    end
 end
-
-def question_params
-    params.permit(:number,:survey_id)
-  end
-end
-
-
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,75 +1,74 @@
 class SurveysController < ApplicationController
-  before_action :set_survey, only: [:show, :edit, :destroy]
+  before_action :set_survey, only: %i[show edit destroy]
 
   def index
-    if UserSurvey.find_by(user_id: current_user.id)
-    @done_flag=true
-  else
-    @done_flag=false
-  end
-    @surveys=Survey.where(company_id: current_user.company_id)
+    @done_flag = if UserSurvey.find_by(user_id: current_user.id)
+                   true
+                 else
+                   false
+               end
+    @surveys = Survey.where(company_id: current_user.company_id)
   end
 
-  def new 
-    @survey=Survey.new
+  def new
+    @survey = Survey.new
   end
 
   def create
-    @survey=Survey.create(survey_params.merge(company_id: current_user.company_id))
+    @survey = Survey.create(survey_params.merge(company_id: current_user.company_id))
     redirect_to survey_path(@survey)
   end
 
   def show
-    @questions=Question.where(survey_id: params[:id]).order(:display_order)
-    @n=1
+    @questions = Question.where(survey_id: params[:id]).order(:display_order)
+    @n = 1
   end
 
-  def edit
-  end
-  
+  def edit; end
+
   def update
     @survey.update(survey_params)
     redirect_to survey_path(@survey), notice: 'Survey was successfully updated.'
   end
 
   def destroy
-    @questions=Question.where!(survey_id: @survey.id)
+    @questions = Question.where(survey_id: @survey.id)
     @questions.each do |q|
-      @textbox=Textbox.find_by!(question_id: q.id)
+      @textbox = Textbox.find_by!(question_id: q.id)
       @textbox.destroy
-      @textarea=Textarea.find_by!(question_id: q.id)
+      @textarea = Textarea.find_by!(question_id: q.id)
       @textarea.destroy
-      @checkboxes=[]
-      @checkbox=Checkbox.find_by!(question_id: q.id)
+      @checkboxes = []
+      @checkbox = Checkbox.find_by!(question_id: q.id)
       @checkboxes.push(@checkbox)
-       @checkboxes.each do |cb|
-         @checkbox_option=CheckboxOption.destroy_all(checkbox_id: cb.id)
-       end
-      @checkbox 
-      @radiobutton=[]
-      @radiobutton=Radiobutton.find_by!(question_id: q.id)
+      @checkboxes.each do |cb|
+        CheckboxOption.destroy_all(checkbox_id: cb.id)
+      end
+      @checkbox.destroy
+      @radiobutton = []
+      @radiobutton = Radiobutton.find_by!(question_id: q.id)
       @radiobutton.push(@radiobutton)
-       @radiobuttons.each do |rb|
-         @radiobutton_option=RudiobuttonOption.destroy_all(radiobutton_id: rb.id)
-       end
+      @radiobuttons.each do |rb|
+        RudiobuttonOption.destroy_all(radiobutton_id: rb.id)
+      end
+      @radiobutton.destroy
     end
+    @questions.destroy
     @survey.destroy
     redirect_to surveys_path, notice: 'Survey was successfully destroyed.'
   end
 
-  def confirm
-  end
-  
-  def result
+  def confirm; end
+
+  def result; end
+
+  private
+
+  def set_survey
+    @survey = Survey.find(params[:id])
   end
 
-private
-def set_survey
-  @survey=Survey.find(params[:id])
-end
-
-def survey_params
+  def survey_params
     params.require(:survey).permit(:title)
-  end
+    end
 end
-
