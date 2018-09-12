1e09abe7af82c63684d420c142eab46b657b5014
keigawa <kei.ogawa@finc.com>
2018-09-12 17:20:23 +0900

    correct except enum

--- app/controllers/answer_checkbox_options_controller.rb
+++ app/controllers/answer_checkbox_options_controller.rb
@@ -2,9 +2,9 @@ class AnswerCheckboxOptionsController < ApplicationController
   def create
     @survey = Survey.find(params[:survey_id])
     checkbox = Checkbox.find_by(question_id: params[:question_id])
-    params[:answer_array].each do |a|
+    params[:answer_array].each do |option_id|
       answer = Answer.create(question_id: params[:question_id], user_id: current_user.id, survey_id: @survey.id)
-      AnswerCheckboxOption.create(check: 'yes', answer_id: answer.id, checkbox_option_id: a)
+      AnswerCheckboxOption.create(check: 'yes', answer_id: answer.id, checkbox_option_id: option_id)
     end
     options = CheckboxOption.where(checkbox_id: checkbox.id)
     options.each do |op|
